61aa959554acf764e364f2b4bd31c47f128cf843
FelixChoi <cjng96@gmail.com>
2023-11-20 10:53:50 +0900

    pub

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/cjng96/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/cjng96/src/flutter_aws_s3_client/example"
+export "FLUTTER_ROOT=/Users/cjng96/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/cjng96/iner/flutter_aws_s3_client/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
